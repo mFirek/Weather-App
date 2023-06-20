b2ad255de1ee892a032bc8c9ab981471082eccd2
Firek <wojtekman6969@gmail.com>
2023-06-20 20:58:44 +0200

    script

--- script.js
+++ script.js
@@ -3,13 +3,11 @@
     var sunnyBg = document.getElementById('sunny_bg');
     var nightBg = document.getElementById('night_bg');
     
-    // Ukryj element sunny_bg na początku
     sunnyBg.style.display = 'none';
     
-    // Ukryj element night_bg po 3 sekundach
     setTimeout(function() {
       nightBg.style.display = 'none';
-      sunnyBg.style.display = 'block'; // Pokaż element sunny_bg
+      sunnyBg.style.display = 'block'; 
     }, 3000);
   });
 
