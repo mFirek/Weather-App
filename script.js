4557b019cf780f0d8dba93a953a4e4927f82d4f7
Firek <wojtekman6969@gmail.com>
2023-06-20 21:00:41 +0200

    fixed script

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-
-  window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function() {
     var sunnyBg = document.getElementById('sunny_bg');
     var nightBg = document.getElementById('night_bg');
     
     sunnyBg.style.display = 'none';
-    
-    setTimeout(function() {
-      nightBg.style.display = 'none';
-      sunnyBg.style.display = 'block'; 
+        
+    setInterval(function() {
+      if (nightBg.classList.contains('active')) {
+        nightBg.style.display = 'none';
+        sunnyBg.style.display = 'block'; 
+        nightBg.classList.remove('active');
+        sunnyBg.classList.add('active');
+      } else {
+        nightBg.style.display = 'block'; 
+        sunnyBg.style.display = 'none';
+        nightBg.classList.add('active');
+        sunnyBg.classList.remove('active');
+      }
     }, 3000);
   });
-
